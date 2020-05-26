55e3633b39d21d688c5c1cc7af1a525d45ac15ba
Mohsin <mohsin@studysmarter.de>
2020-05-26 18:47:33 +0500

    Pending request page is fixdd

--- src/pages/dashboard/PendingRequests.js
+++ src/pages/dashboard/PendingRequests.js
@@ -11,26 +11,18 @@ class PendingRequests extends React.Component {
 
                 <div className="d-sm-flex align-items-center justify-content-between mb-4">
 
-                    <h2 className="m-0 font-weight-bold text-primary">Pending Requests</h2>
-
-
+                    <h2 className="m-0 font-weight-bold">Pending Requests</h2>
 
                 </div>
 
+                <div id="border" className="">
 
-
-                <div id="border" className="col-xl-12 col-lg-5">
-
-                    <div className="card shadow mb-4">
-
-
+                    <div className="card shadow mb-4 rounded-20 border-yellow border-5">
                         <div className="card-body">
-
-                            <div className="pt-4 pb-2" style={{ padding: "0 !important" }}>
-
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown.</p>
-                                <a href="#" id="accept" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">Accept</a>
-                                <a href="#" id="denny" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">Deny</a>
+                            <div className="pb-2" style={{ padding: "0 !important" }}>
+                                <p className="text-secondary">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown.</p>
+                                <button id="accept" className="rounded-30 px-5 text-white d-sm-inline-block btn btn-sm btn-primary shadow-sm mr-3">Accept</button>
+                                <button className="rounded-30 px-5 text-white d-sm-inline-block btn btn-sm shadow-sm btn-danger">Deny</button>
 
                             </div>
 
@@ -40,18 +32,16 @@ class PendingRequests extends React.Component {
 
                 </div>
 
-                <div id="border" className="col-xl-12 col-lg-5">
+                <div id="border" className="">
 
-                    <div className="card shadow mb-4">
-
-
+                    <div className="card shadow mb-4 rounded-20 border-yellow border-5">
                         <div className="card-body">
 
-                            <div className="pt-4 pb-2" style={{ padding: "0 !important" }}>
+                            <div className="pb-2" style={{ padding: "0 !important" }}>
 
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown.</p>
-                                <a href="#" id="accept" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">Accept</a>
-                                <a href="#" id="denny" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">Deny</a>
+                                <p className="text-secondary">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown.</p>
+                                <button id="accept" className="rounded-30 px-5 text-white d-sm-inline-block btn btn-sm btn-primary shadow-sm mr-3">Accept</button>
+                                <button className="rounded-30 px-5 text-white d-sm-inline-block btn btn-sm shadow-sm btn-danger">Deny</button>
 
                             </div>
 
@@ -60,17 +50,17 @@ class PendingRequests extends React.Component {
                     </div>
 
                 </div>
-                <div id="border" className="col-xl-12 col-lg-5">
+                <div id="border" className="">
 
-                    <div className="card shadow mb-4">
+                    <div className="card shadow mb-4 rounded-20 border-yellow border-5">
 
                         <div className="card-body">
 
-                            <div className="pt-4 pb-2" style={{ padding: "0 !important" }}>
+                            <div className="pb-2" style={{ padding: "0 !important" }}>
 
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown.</p>
-                                <a href="#" id="accept" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">Accept</a>
-                                <a href="#" id="denny" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">Deny</a>
+                                <p className="text-secondary">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown.</p>
+                                <button id="accept" className="rounded-30 px-5 text-white d-sm-inline-block btn btn-sm btn-primary shadow-sm mr-3">Accept</button>
+                                <button className="rounded-30 px-5 text-white d-sm-inline-block btn btn-sm shadow-sm btn-danger">Deny</button>
 
                             </div>
 
@@ -81,23 +71,15 @@ class PendingRequests extends React.Component {
                 </div>
                 <div id="btn-top" className="mt-4 text-center small" style={{ marginBottom: "50px" }}>
 
-                    <a href="#" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">See more</a>
+                    <button href="#" className="rounded-30 px-5 text-white d-sm-inline-block btn btn-sm btn-primary shadow-sm">See more</button>
 
                 </div>
             </div>
-
-
-)
-
-
+        )
 
 
     }
 
 
-
-
 }
-
-
 export default PendingRequests;
