e789d9ed42190910ecb5687b092adeb7ea826283
Mohsin <mohsin@studysmarter.de>
2020-06-01 21:37:06 +0500

    Auth pages finished

--- pwa/src/auth/pages/Login.jsx
+++ pwa/src/auth/pages/Login.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { Form } from 'react-bootstrap';
 import { faGoogle, faFacebook } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Login.css'
 export function Login({ match }) {
     return (
         <div className="login">
-            <div className="bg-gradient-primary">
+            <div className="bg-gradient-primary d-flex justify-content-center align-items-center">
                 <div className="container">
 
                     <div className="row justify-content-center">
 
                         <div className="col-xl-10 col-lg-12 col-md-9">
 
-                            <div className="card o-hidden border-0 shadow-lg my-5">
+                            <div className="card o-hidden border-0 shadow-lg my-5 overflow-hidden rounded-5">
                                 <div className="card-body p-0">
-                                    <div className="row">
+                                    <div className="row mx-0">
                                         <div className="col-lg-6" style={{
                                             background: "url(https://source.unsplash.com/K4mSJ7kc0As/600x800)",
                                             backgroundPosition: "center",
@@ -28,48 +29,40 @@ export function Login({ match }) {
                                                 <div className="text-center">
                                                     <h1 className="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                                 </div>
-                                                <div className="alert alert-success" role="alert">
-                                                </div>
-
-                                                <div className="alert alert-danger" role="alert">
-                                                </div>
-                                                <div className="alert alert-danger" role="alert">
-                                                </div>
-
                                                 <form className="user" method="post">
                                                     <div className="form-group">
-                                                        <input type="email" name="email" className="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..." value="" />
+                                                        <input type="email" name="email" className="py-4 form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..." />
                                                         <p style={{ color: "red" }}> </p>
                                                     </div>
 
                                                     <div className="form-group">
-                                                        <input type="password" name="password" className="form-control form-control-user" id="exampleInputPassword" placeholder="Password" value="" />
+                                                        <input type="password" name="password" className="py-4 form-control form-control-user" id="exampleInputPassword" placeholder="Password" />
                                                         <p style={{ color: "red" }}> </p>
                                                     </div>
-                                                    <div className="form-group">
-                                                        <div className="custom-control custom-checkbox small">
-                                                            <input type="checkbox" name="checkbox" className="custom-control-input" id="customCheck" checked />
-                                                            <label className="custom-control-label" for="customCheck">Remember Me</label>
+                                                    <div className="form-group remember-me">
+                                                        <div className="custom-control custom-checkbox small d-flex align-items-center">
+                                                            <input type="checkbox" name="checkbox" className="py-4 custom-control-input" id="customCheck" />
+                                                            <label className="custom-control-label" htmlFor="customCheck">Remember Me</label>
                                                         </div>
                                                     </div>
-                                                    <Link to="/dashboard/overview" className="btn btn-primary btn-user btn-block">Login</Link>
+                                                    <Link to="/brand" className="btn btn-user btn-block text-white no-focus-effects">Login</Link>
 
                                                 </form>
                                                 <hr />
-                                                <Link to="/" className="btn btn-google btn-user btn-block">
-                                                    <FontAwesomeIcon icon={faGoogle} />
+                                                <Link to="/" className="btn btn-google btn-user btn-block no-focus-effects d-flex justify-content-center align-items-center">
+                                                    <FontAwesomeIcon className="social-icon" icon={faGoogle} />
                                                     Login with Google
                                                 </Link>
-                                                <Link to="/" className="btn btn-facebook btn-user btn-block">
-                                                    <FontAwesomeIcon icon={faFacebook} />
+                                                <Link to="/" className="btn btn-facebook btn-user btn-block no-focus-effects d-flex justify-content-center align-items-center">
+                                                    <FontAwesomeIcon className="social-icon" icon={faFacebook} />
                                                 Login with Facebook
                                                 </Link>
                                                 <hr />
                                                 <div className="text-center">
-                                                    <Link className="small" to="/">Forgot Password?</Link>
+                                                    <Link className="small text-dark-blue" to="/">Forgot Password?</Link>
                                                 </div>
                                                 <div className="text-center">
-                                                    <Link className="small" to="/auth/signup">Create an Account!</Link>
+                                                    <Link className="small text-dark-blue" to="/auth/signup">Create an Account!</Link>
                                                 </div>
                                             </div>
                                         </div>
--- pwa/src/auth/pages/Signup.jsx
+++ pwa/src/auth/pages/Signup.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import './Login.css';
+import './Signup.css';
 import { faGoogle, faFacebook } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 export function Signup({ match }) {
     return (
-        <div className="signin">
-            <div className="bg-gradient-primary">
+        <div className="signup singup-container">
+            <div className="bg-gradient-primary py-5">
                 <div className="container">
-
-                    <div className="card o-hidden border-0 shadow-lg my-5">
+                    <div className="card o-hidden border-0 shadow-lg">
                         <div className="card-body p-0">
                             <div className="row">
                                 <div className="col-lg-12">
@@ -23,74 +22,65 @@ export function Signup({ match }) {
                                         <form className="user" method="post">
                                             <div className="form-group row">
                                                 <div className="col-sm-6 mb-3 mb-sm-0">
-                                                    <input type="text" className="form-control form-control-user" id="exampleFirstName" name="First_Name" placeholder="First Name" value="" />
-                                                    <p style={{ color: "red" }}> </p>
+                                                    <input type="text" className="py-4 pl-3 form-control form-control-user" id="exampleFirstName" name="First_Name" placeholder="First Name" value="" />
                                                 </div>
 
                                                 <div className="col-sm-6">
-                                                    <input type="text" className="form-control form-control-user" id="exampleLastName" name="Last_Name" placeholder="Last Name" value="" />
-                                                    <p style={{ color: "red" }}> </p>
+                                                    <input type="text" className="py-4 pl-3 form-control form-control-user" id="exampleLastName" name="Last_Name" placeholder="Last Name" value="" />
                                                 </div>
                                             </div>
                                             <div className="form-group">
-                                                <input type="text" className="form-control form-control-user" id="exampleInputEmail" name="phone" placeholder="Telephone number(optional)" value="" />
+                                                <input type="text" className="py-4 pl-3 form-control form-control-user" id="exampleInputEmail" name="phone" placeholder="Telephone number(optional)" value="" />
                                             </div>
 
                                             <div className="form-group row">
                                                 <div className="col-sm-6">
-                                                    <input type="text" className="form-control form-control-user" id="exampleLastName" name="company" placeholder="Company Name" value="" />
-                                                    <p style={{ color: "red" }}> </p>
+                                                    <input type="text" className="py-4 pl-3 form-control form-control-user" id="exampleLastName" name="company" placeholder="Company Name" value="" />
                                                 </div>
 
                                                 <div className="col-sm-6">
-                                                    <input type="text" className="form-control form-control-user" id="exampleLastName" name="role" placeholder="Your role in company" value="" />
-                                                    <p style={{ color: "red" }}> </p>
+                                                    <input type="text" className="py-4 pl-3 form-control form-control-user" id="exampleLastName" name="role" placeholder="Your role in company" value="" />
                                                 </div>
                                             </div>
 
 
                                             <div className="form-group">
-                                                <input type="email" className="form-control form-control-user" id="exampleInputEmail" name="email" placeholder="Email Address" value="" />
-                                                <p style={{ color: "red" }}> </p>
+                                                <input type="email" className="py-4 pl-3 form-control form-control-user" id="exampleInputEmail" name="email" placeholder="Email Address" value="" />
                                             </div>
                                             <div className="form-group row">
                                                 <div className="col-sm-6 mb-3 mb-sm-0">
-                                                    <input type="password" className="form-control form-control-user" id="exampleInputPassword" name="password" placeholder="Password" value="" />
-                                                    <p style={{ color: "red" }}></p>
+                                                    <input type="password" className="py-4 pl-3 form-control form-control-user" id="exampleInputPassword" name="password" placeholder="Password" value="" />
                                                 </div>
                                                 <div className="col-sm-6">
-                                                    <input type="password" className="form-control form-control-user" id="exampleRepeatPassword" name="confirm" placeholder="Repeat Password" value="" />
-                                                    <p style={{ color: "red" }}> </p>
+                                                    <input type="password" className="py-4 pl-3 form-control form-control-user" id="exampleRepeatPassword" name="confirm" placeholder="Repeat Password" value="" />
                                                 </div>
                                             </div>
                                             <div className="form-group">
-                                                <select className="browser-default custom-select" name="admin_type">
-
+                                                <select className="py-4 pl-3 browser-default custom-select" name="admin_type">
                                                     <option value="1">I'm a Brand</option>
                                                     <option value="2">I'm a Influencer</option>
                                                 </select>
 
                                             </div>
-                                            <Link to="/dashboard/overview" className="btn btn-primary btn-user btn-block">Click to signup</Link>
+                                            <Link to="/dashboard/overview" className="btn btn-user btn-block text-white">Register Account</Link>
                                             <hr />
-                                            <Link to="/" className="btn btn-google btn-user btn-block">
-
-                                                <FontAwesomeIcon icon={faGoogle} />
+                                            <Link to="/" className="btn btn-google btn-block">
+                                                <FontAwesomeIcon className="social-icon" icon={faGoogle} />
                                                  Register with Google
                                             </Link>
-                                            <Link to="/" className="btn btn-facebook btn-user btn-block">
+                                            <Link to="/" className="btn btn-facebook btn-block">
 
-                                                <FontAwesomeIcon icon={faFacebook} />
+                                                <FontAwesomeIcon className="social-icon" icon={faFacebook} />
 
                                             Register with Facebook
                                             </Link>
                                         </form>
                                         <hr />
                                         <div className="text-center">
-                                            <Link className="small" to="/">Forgot Password?</Link>
+                                            <Link className="small text-dark-blue" to="/">Forgot Password?</Link>
                                         </div>
                                         <div className="text-center">
-                                            <Link className="small" to="/auth/login">Already have an account? Login!</Link>
+                                            <Link className="small text-dark-blue" to="/auth/login">Already have an account? Login!</Link>
                                         </div>
                                         <div>
                                         </div>
