a43b0ebc8b5262391b856da16ed2f004002d754a
mohsini172 <mohsini172@gmail.com>
2020-06-08 19:00:01 +0500

    formatting

--- pwa/src/auth/pages/Login.jsx
+++ pwa/src/auth/pages/Login.jsx
@@ -4,12 +4,11 @@ import { faGoogle, faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-i
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { GoogleLogin } from 'react-google-login';
 import ReactDOM from 'react-dom';
-import {Button} from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props'
 import './Login.css'
 import { LinkedIn } from 'react-linkedin-login-oauth2';
 import { LinkedInPopUp } from 'react-linkedin-login-oauth2';
-
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const responseGoogle = (response) => {
@@ -18,14 +17,11 @@ const responseGoogle = (response) => {
 const responseFacebook = (response) => {
     console.log(response);
 }
-
 export class Login extends React.Component {
     state = {
         code: '',
         errorMessage: '',
     };
-
-
     handleSuccess = (data) => {
         this.setState({
             code: data.code,
@@ -33,9 +29,7 @@ export class Login extends React.Component {
         },
             console.log(data)
         );
-
     }
-
     handleFailure = (error) => {
         this.setState({
             code: '',
@@ -46,16 +40,12 @@ export class Login extends React.Component {
     }
     render() {
         const { code, errorMessage } = this.state;
-
         return (
             <div className="login">
                 <div className="bg-gradient-primary d-flex justify-content-center align-items-center">
                     <div className="container">
-
                         <div className="row justify-content-center">
-
                             <div className="col-xl-10 col-lg-12 col-md-9">
-
                                 <div className="card o-hidden border-0 shadow-lg my-5 overflow-hidden rounded-5">
                                     <div className="card-body p-0">
                                         <div className="row mx-0">
@@ -64,7 +54,6 @@ export class Login extends React.Component {
                                                 backgroundPosition: "center",
                                                 backgroundSize: "cover"
                                             }}>
-
                                             </div>
                                             <div className="col-lg-6">
                                                 <div className="p-5">
@@ -76,7 +65,6 @@ export class Login extends React.Component {
                                                             <input type="email" name="email" className="py-4 form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..." />
                                                             <p style={{ color: "red" }}> </p>
                                                         </div>
-
                                                         <div className="form-group">
                                                             <input type="password" name="password" className="py-4 form-control form-control-user" id="exampleInputPassword" placeholder="Password" />
                                                             <p style={{ color: "red" }}> </p>
@@ -88,7 +76,6 @@ export class Login extends React.Component {
                                                             </div>
                                                         </div>
                                                         <Link to="/brand" className="btn btn-user btn-block text-white no-focus-effects">Login</Link>
-
                                                     </form>
                                                     <hr />
                                                     <GoogleLogin
@@ -96,7 +83,6 @@ export class Login extends React.Component {
                                                         render={renderProps => (
                                                             <Button
                                                                 onClick={renderProps.onClick} disabled={renderProps.disabled}
-
                                                                 className="btn btn-google btn-user btn-block no-focus-effects d-flex justify-content-center align-items-center">
                                                                 <FontAwesomeIcon className="social-icon" icon={faGoogle} />
                                                     Login with Google
@@ -106,14 +92,12 @@ export class Login extends React.Component {
                                                         onFailure={responseGoogle}
                                                         cookiePolicy={'single_host_origin'}
                                                     />
-
                                                     <FacebookLogin
                                                         appId="263197894950161"
                                                         autoLoad
                                                         callback={responseFacebook}
                                                         render={renderProps => (
                                                             <Button
-
                                                                 onClick={renderProps.onClick}
                                                                 className="btn btn-facebook btn-user btn-block no-focus-effects d-flex justify-content-center align-items-center">
                                                                 <FontAwesomeIcon className="social-icon" icon={faFacebook} />
@@ -125,28 +109,20 @@ export class Login extends React.Component {
                                                         clientId="865ntwzmhauy0w"
                                                         onFailure={this.handleFailure}
                                                         onSuccess={
-                                                             function(data) {
+                                                            function (data) {
                                                                 console.log(data)
-
                                                                 this.setState({
                                                                     code: data.code,
                                                                     errorMessage: '',
                                                                 },
                                                                 );
-                                                        
                                                             }
-
-
-
                                                         }
                                                         scope="r_liteprofile"
                                                         redirectUri="http://localhost:3000/auth/login"
                                                         renderElement={({ onClick, disabled }) => (
-
                                                             <Button
-
                                                                 onClick={onClick} disabled={disabled}
-
                                                                 className="btn btn-linkedin btn-block no-focus-effects d-flex justify-content-center align-items-center">
                                                                 <FontAwesomeIcon className="social-icon" icon={faLinkedin} />
                                                     Login with LinkedIn
@@ -156,7 +132,6 @@ export class Login extends React.Component {
                                                     {!code && <div>No code</div>}
                                                     {code && <div>Code: {code}</div>}
                                                     {errorMessage && <div>{errorMessage}</div>}
-                                                    
                                                     <hr />
                                                     <div className="text-center">
                                                         <Link className="small text-dark-blue" to="/">Forgot Password?</Link>
@@ -169,16 +144,11 @@ export class Login extends React.Component {
                                         </div>
                                     </div>
                                 </div>
-
                             </div>
-
                         </div>
-
                     </div>
-
                 </div>
             </div>
         );
-
     }
 }
