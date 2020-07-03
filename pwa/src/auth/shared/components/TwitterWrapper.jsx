2405dacde7d6013a3eabf29dcc1c980f1f12aeae
Mohsin Ammar <mohsini172@gmail.com>
2020-07-03 10:13:48 +0500

    little tweak

--- pwa/src/auth/shared/components/TwitterWrapper.jsx
+++ pwa/src/auth/shared/components/TwitterWrapper.jsx
@@ -12,16 +12,27 @@ export default function TwitterWrapper() {
   const [token, setToken] = useState("");
   const [tokenSecret, setTokenSecret] = useState("")
   const [success, setSuccess] = useState(false)
+  function listenMessage(msg) {
+    console.log(msg);
+}
+
 
   function handleClick() {
     const host = window.location.protocol + '//' + window.location.hostname + ':' + window.location.port;
     const oauthWindow = window.open(encodeURI(`https://api.twitter.com/oauth/authenticate?oauth_token=${oauth_access}`));
+    localStorage.setItem("mytime", Date.now());
 
     var timer = setInterval(function () {
         if (oauthWindow.closed) {
             clearInterval(timer);
-            const redirectUrl = new URL(localStorage.getItem('oAuthRedirectUrl'));
-            const searchParams = redirectUrl.searchParams;
+            if (window.addEventListener) {
+              window.addEventListener("message", listenMessage, false);
+          } else {
+              window.attachEvent("onmessage", listenMessage);
+          }
+           // console.log(localStorage);
+           // const redirectUrl = new URL(localStorage.getItem('oAuthRedirectUrl',window.location.href));
+            //const searchParams = redirectUrl.searchParams;
             
         }
     }, 1000);
@@ -39,7 +50,7 @@ export default function TwitterWrapper() {
   }, [])
   return (
     <div>
-<button onClick={handleClick}>
+<button onClick={handleClick} type="button" >
   <FontAwesomeIcon icon={faTwitter} />
 </button>
      
