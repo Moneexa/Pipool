115963ad45fa20151c1a1673db96f887d163f238
Mohsin Ammar <mohsini172@gmail.com>
2020-07-23 13:48:22 +0500

    facebook oauth

--- pwa/src/influence/shared/components/FacebookVerify.jsx
+++ pwa/src/influence/shared/components/FacebookVerify.jsx
@@ -2,43 +2,58 @@ import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useStoreActions } from 'easy-peasy';
 import React from 'react';
+import axios from 'axios'
 import config from '../../../config.json';
 
 
 
 export function FacebookVerify() {
-    // const loginFacebook = useStoreActions(actions => actions.user.loginFacebook);
+    const authFacebook = useStoreActions(actions => actions.channels.authFacebook);
 
     function openPopup() {
-
         const host = window.location.protocol + '//' + window.location.hostname + ':' + window.location.port;
-        const oauthWindow = window.open(encodeURI(`${config.facebook.uri}/?redirect_uri=${host}${config.facebook.redirectURI}&client_id=${config.temp.AppId}&scope=${config.facebook.scope}&response_type=token&state={"{st=state123abc,ds=123456789}"}`));
+        const oauthWindow = window.open(encodeURI(`${config.instagram.uri}/?redirect_uri=${host}${config.instagram.redirectURI}&client_id=${config.instagram.appId}&scope=${config.instagram.scope}&response_type=token&state={"{st=state123abc,ds=123456789}"}`));
 
         var timer = setInterval(function () {
+            console.log("here")
             if (oauthWindow.closed) {
                 clearInterval(timer);
-                const redirectUrl = new URL(localStorage.getItem('oAuthRedirectUrl'));
+                const redirectUrl = new URL(localStorage.getItem('oAuthRedirectUrl').replace('?#', '?'));
                 const searchParams = redirectUrl.searchParams;
                 console.log(searchParams);
-                
-                const code = searchParams.get('access_token');
-                // const error = searchParams.get('error');
 
-                console.log(code);
+                const code = searchParams.get('access_token');
+               // setToken(code);
+                // const error = searchParams.get('error');
+                console.log(code)
+                axios.get(`https://graph.facebook.com/v7.0/me/accounts?fields=instagram_business_account,name,id&access_token=${code}`)
+                    .then(res => {
+                        console.log(res.data.data[0]["id"])
+                        const payload={
+                            token:code,
+                            id: res.data.data[0]["id"]
+                        }
+                        authFacebook(payload)
+                        //setAccountsList(res.data.data)
+                        //setShowPopup(true);
+                    })
+                    .catch(console.error)
 
                 // loginFacebook(code);
+
             }
         }, 1000);
+        
     }
 
 
     return (
 
-        <button
+        <button type="button"
             onClick={() => openPopup()}
-            className="btn btn-facebook btn-block no-focus-effects d-flex justify-content-center align-items-center ">
-            <FontAwesomeIcon className="social-icon" icon={faFacebook} />
-                Login with Facebook
+            className="btn btn-primary rounded-20 text-white">
+            <FontAwesomeIcon icon={faFacebook} />
+            &nbsp; Facebook +
         </button>
 
     );
