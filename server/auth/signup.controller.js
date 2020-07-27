bc204abfb85b1c96051f7901fc6689195ee0d770
Mohsin Ammar <mohsini172@gmail.com>
2020-07-27 22:07:52 +0500

    crypto added

--- server/auth/signup.controller.js
+++ server/auth/signup.controller.js
@@ -1,16 +1,17 @@
 const UsertModel = require('./user.model.js');
 const config = require('../config.json');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const { validationResult } = require('express-validator');
 const axios = require('axios');
 var querystring = require('querystring');
 var nodemailer = require('nodemailer');
+const crypto = require('crypto')
 
 var transporter = nodemailer.createTransport({
     service: 'gmail',
-    
+
     auth: {
         user: config.smtp.email,
         pass: config.smtp.password
@@ -53,11 +54,11 @@ function signup(req, res) {
 
     signToken(id, email, undefined, undefined, undefined, function (err, token) {
         // console.log(id);
-       // console.log(token)
-       /* if (err) {
-            console.log(err);
-            return res.sendStatus(200);
-        }*/
+        // console.log(token)
+        /* if (err) {
+             console.log(err);
+             return res.sendStatus(200);
+         }*/
         var mailOptions = {
             from: config.smtp.email,
             to: email,
@@ -71,7 +72,7 @@ function signup(req, res) {
                 console.log(error);
                 res.status(500).send("Something went wrong while sending email");
             } else {*/
-                res.status(201).send();
+            res.status(201).send();
             // }
         });
     });
@@ -89,26 +90,27 @@ function finishSignup(req, res) {
     const role = req.body.role;
     const id = res.locals.user.id;
     const email = res.locals.user.email;
-    bcrypt.hash(req.body.password, 8)
-        .then((password) => {
-            return UsertModel.updateOne(
-                { _id: id },
-                {
-                    email,
-                    fullName,
-                    phone,
-                    company,
-                    designation,
-                    password,
-                    role
-                },
-                {
-                    upsert: true
-                }
-            );
-        })
-        .then(() => {
+    //bcrypt.hash(req.body.password, 8)
+    // .then((password) => {
+    crypto.pbkdf2(req.body.password, 'salt', 100, 64, 'sha512', (err, derivedKey) => {
 
+        const user = UsertModel.updateOne(
+            { _id: id },
+            {
+                email,
+                fullName,
+                phone,
+                company,
+                designation,
+                derivedKey,
+                role
+            },
+            {
+                upsert: true
+            }
+        );
+
+        try {
             signToken(
                 id,
                 undefined,
@@ -134,10 +136,10 @@ function finishSignup(req, res) {
                     res.status(200).send(obj)
                 }
             )
-        })
-        .catch(error => {
+        }
+        catch (error) {
             console.error(error);
             res.status(500).send("Something went wrong");
-        });
-
+        };
+    })
 }
