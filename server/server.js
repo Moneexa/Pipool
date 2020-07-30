7c002ae7a9029636ff5e9dafd3a8f1113038673f
Moneexa <14besemsyed@seecs.edu.pk>
2020-07-30 19:31:23 +0500

    added config and root files

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser=require('body-parser');
+var mongoose = require('./database/mongoose');
+const app = express()
+
+var cors = require('cors');
+
+
+const loginRoute = require('./auth/login.router'); // Imports routes for the products
+const signupRoute = require('./auth/signup.router'); // Imports routes for the products
+const insightsRoute = require('./insights/insightsRouter')
+const brandRoute = require('./brand/brandRoutes')
+
+const campaignRoute = require('./campaign/campaignRoutes')
+const twitter= require('./channel/channelRoutes');
+
+var port = process.env.PORT || 4242;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.get('/', (req, res) => res.send('Hello World with Express'));
+app.use(cors());
+
+app.use('/api/auth/login', loginRoute);
+app.use('/api/auth/signup', signupRoute);
+app.use('/api/brands', brandRoute);
+app.use('/api/campaigns', campaignRoute);
+app.use('/api/influencers/channels', twitter);
+app.use('/api/channels/insights', insightsRoute )
+
+app.listen(port, () => console.log('Server running on port 4242!'))
