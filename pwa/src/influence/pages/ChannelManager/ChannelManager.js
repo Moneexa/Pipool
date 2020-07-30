69cede7e74e70622c6c0a5d8b5e6a51af0a5d86c
Moneexa <14besemsyed@seecs.edu.pk>
2020-07-30 19:43:17 +0500

    social media apps integration

--- pwa/src/influence/pages/ChannelManager/ChannelManager.js
+++ pwa/src/influence/pages/ChannelManager/ChannelManager.js
@@ -1,213 +1,82 @@
-import React from 'react';
-import { faFacebookF, faTwitter, faLinkedinIn, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import React, { useState } from 'react';
+import { useStoreActions, useStoreState } from 'easy-peasy';
+import { faFacebookF, faTwitter, faInstagram, faYoutube , faTiktok } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './ChannelManager.css'
-class Channel extends React.Component {
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom'
 
-    render() {
+function Channel() {
+    const channels = useStoreState(state => state.channels.channels);
+    const listChannels = useStoreActions(actions => actions.channels.listChannels);
 
-        return (
+    useEffect(() => {
+        listChannels();
+        console.log(channels)
+    }, [])
 
-            <div className="channel">
-
-
-                <div className="d-sm-flex align-items-center justify-content-between mb-4">
-
-                    <h2 className="m-0 font-weight-bold text-primary">Channel</h2>
-                </div>
-
-                <div className="row">
-
-                    <div id="border" className="col-xl-4   col-lg-7">
-
-                        <div className="card shadow mb-4">
-                            <div className="card-body">
-
-                                <img alt="" src="../img/users_profile.jpg" />
-                                <h4>TIMOTHY</h4>
-                                <p><b>Project Manager</b></p>
-                                <p style={{ marginBottom: "50px" }}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
-                                <div className="text-center d-none d-md-inline">
-                                    <button className="btn btn-floating btn-lg btn-fb" type="button">
-                                        <FontAwesomeIcon icon={faFacebookF} />
-                                    </button>
-                                    <button className="btn btn-floating btn-lg btn-tw" type="button">
-                                        <FontAwesomeIcon icon={faTwitter} />
-                                    </button>
-                                    <button className="btn btn-floating btn-lg btn-li" type="button">
-                                        <FontAwesomeIcon icon={faLinkedinIn} />
-                                    </button>
-                                    <button className="btn btn-floating btn-lg btn-ins" type="button">
-                                        < FontAwesomeIcon icon={faInstagram} />
-                                    </button>
-
-                                </div>
-
-                            </div>
-
-                        </div>
-
-                    </div>
-
-                    <div id="border" className="col-xl-4    col-lg-7">
-
-                        <div className="card shadow mb-4">
-                            <div className="card-body">
-
-                                <img alt="" src="../img/users_profile.jpg" />
-                                <h4>TIMOTHY</h4>
-                                <p><b>Project Manager</b></p>
-                                <p style={{ marginBottom: "50px" }}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
-                                <div className="text-center d-none d-md-inline">
-                                    <button className="btn btn-floating btn-lg btn-fb" type="button">
-                                        <FontAwesomeIcon icon={faFacebookF} />
-                                    </button>
-                                    <button className="btn btn-floating btn-lg btn-tw" type="button">
-                                        <FontAwesomeIcon icon={faTwitter} />
-                                    </button>
-                                    <button className="btn btn-floating btn-lg btn-li" type="button">
-                                        <FontAwesomeIcon icon={faLinkedinIn} />
-                                    </button>
-                                    <button className="btn btn-floating btn-lg btn-ins" type="button">
-                                        < FontAwesomeIcon icon={faInstagram} />
-                                    </button>
-                                </div>
-
-                            </div>
-
-                        </div>
-
-                    </div>
-
-                    <div id="border" className="col-xl-4   col-lg-7">
-
-                        <div className="card shadow mb-4">
-
-                            <div className="card-body">
-
-                                <img alt="" src="../img/users_profile.jpg" />
-                                <h4>TIMOTHY</h4>
-                                <p><b>Project Manager</b></p>
-                                <p style={{ marginBottom: "50px" }}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
-                                <div className="text-center d-none d-md-inline">
-                                    <button className="btn btn-floating btn-lg btn-fb" type="button">
-                                        <FontAwesomeIcon icon={faFacebookF} />
-                                    </button>
-                                    <button className="btn btn-floating btn-lg btn-tw" type="button">
-                                        <FontAwesomeIcon icon={faTwitter} />
-                                    </button>
-                                    <button className="btn btn-floating btn-lg btn-li" type="button">
-                                        <FontAwesomeIcon icon={faLinkedinIn} />
-                                    </button>
-                                    <button className="btn btn-floating btn-lg btn-ins" type="button">
-                                        < FontAwesomeIcon icon={faInstagram} />
-                                    </button>
-                                </div>
-
-                            </div>
-
-                        </div>
-
-                    </div>
-
-                </div>
-                <div className="row">
-
-                    <div id="border" className="col-xl-4   col-lg-7">
-
-                        <div className="card shadow mb-4">
-
-                            <div className="card-body">
-
-                                <img alt="" src="../img/users_profile.jpg" />
-                                <h4>TIMOTHY</h4>
-                                <p><b>Project Manager</b></p>
-                                <p style={{ marginBottom: "50px" }}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
-                                <div className="text-center d-none d-md-inline">
-                                    <button className="btn btn-floating btn-lg btn-fb" type="button">
-                                        <FontAwesomeIcon icon={faFacebookF} />
-                                    </button>
-                                    <button className="btn btn-floating btn-lg btn-tw" type="button">
-                                        <FontAwesomeIcon icon={faTwitter} />
-                                    </button>
-                                    <button className="btn btn-floating btn-lg btn-li" type="button">
-                                        <FontAwesomeIcon icon={faLinkedinIn} />
-                                    </button>
-                                    <button className="btn btn-floating btn-lg btn-ins" type="button">
-                                        < FontAwesomeIcon icon={faInstagram} />
-                                    </button>
-
-                                </div>
-
-                            </div>
-
-                        </div>
-                    </div>
-                    <div id="border" className="col-xl-4   col-lg-7">
-                        <div className="card shadow mb-4">
-                           <div className="card-body">
-                                <img alt="" src="../img/users_profile.jpg" />
-                                <h4>TIMOTHY</h4>
-                                <p><b>Project Manager</b></p>
-                                <p style={{ marginBottom: "50px" }}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
-                                <div className="text-center d-none d-md-inline">
-                                    <button className="btn btn-floating btn-lg btn-fb" type="button">
-                                        <FontAwesomeIcon icon={faFacebookF} />
-                                    </button>
-                                    <button className="btn btn-floating btn-lg btn-tw" type="button">
-                                        <FontAwesomeIcon icon={faTwitter} />
-                                    </button>
-                                    <button className="btn btn-floating btn-lg btn-li" type="button">
-                                        <FontAwesomeIcon icon={faLinkedinIn} />
-                                    </button>
-                                    <button className="btn btn-floating btn-lg btn-ins" type="button">
-                                        < FontAwesomeIcon icon={faInstagram} />
-                                    </button>
-                                </div>
-
-                            </div>
-
-                        </div>
-
-                    </div>
-                    <div id="border" className="col-xl-4   col-lg-7">
-
-                        <div className="card shadow mb-4">
-                            <div className="card-body">
-
-                                <img alt="" src="../img/users_profile.jpg" />
-                                <h4>TIMOTHY</h4>
-                                <p><b>Project Manager</b></p>
-                                <p style={{ marginBottom: "50px" }}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
-                                <div className="text-center d-none d-md-inline">
-                                    <button className="btn btn-floating btn-lg btn-fb" type="button">
-                                        <FontAwesomeIcon icon={faFacebookF} />
-                                    </button>
-                                    <button className="btn btn-floating btn-lg btn-tw" type="button">
-                                        <FontAwesomeIcon icon={faTwitter} />
-                                    </button>
-                                    <button className="btn btn-floating btn-lg btn-li" type="button">
-                                        <FontAwesomeIcon icon={faLinkedinIn} />
-                                    </button>
-                                    <button className="btn btn-floating btn-lg btn-ins" type="button">
-                                        < FontAwesomeIcon icon={faInstagram} />
-                                    </button>
-                                </div>
-
-                            </div>
-
-                        </div>
-
-                    </div>
-
-                </div>
+    return (
+        <div className="channel">
+            <div className="d-sm-flex align-items-center justify-content-between mb-4">
+                <h2 className="m-0 font-weight-bold text-primary">Channel</h2>
             </div>
-            
+            <div className="row">
+                {channels.map((value, index) =>
+                    <div id="border" key={index} className="col-xl-4   col-lg-7">
+                        <div className="card shadow mb-4">
+                            <div className="card-body">
+                                <img alt="" src="../img/users_profile.jpg" />
+                                <h4>{value.channelName}</h4>
+                                <p><b>Followers: {value.followers}</b></p>
+                                <p style={{ marginBottom: "50px" }}>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+                                <div className="text-center d-none d-md-inline">
+                                    {
+                                        value.channelType === "facebook" ?
+                                            <Link to={`/influencer/channel-insights/${value.channelType}/${value.channelId}`}>
 
+                                                <button className="btn btn-floating btn-lg btn-fb" type="button" >
+                                                    <FontAwesomeIcon icon={faFacebookF} />
+                                                </button></Link> : ''
+                                    }
+                                    {
+                                        value.channelType === "twitter" ?
+                                            <Link to={`/influencer/channel-insights/${value.channelType}/${value.channelId}`}>
 
-        )
-    }
+                                                <button className="btn btn-floating btn-lg btn-tw" type="button">
+                                                    <FontAwesomeIcon icon={faTwitter} />
+                                                </button></Link> : ''
+                                    }
+                                    {
+                                        value.channelType === "tiktok" ?
+                                            <Link to={`/influencer/channel-insights/${value.channelType}/${value.channelName}`}>
 
+                                                <button className="btn btn-floating btn-lg btn-tw" type="button">
+                                                    <FontAwesomeIcon icon={faTiktok} />
+                                                </button></Link> : ''
+                                    }
+
+                                    {
+                                        value.channelType === "instagram" ?
+                                            <Link to={`/influencer/channel-insights/${value.channelType}/${value.channelId}`}>
+                                                <button className="btn btn-floating btn-lg btn-ins" type="button">
+                                                    < FontAwesomeIcon icon={faInstagram} />
+                                                </button></Link> : ''
+                                    }
+                                    {
+                                        value.channelType === "youtube" ?
+                                            <Link to={`/influencer/channel-insights/${value.channelType}/${value.channelId}`}>
+
+                                                <button className="btn btn-floating btn-lg btn-ins" type="button">
+                                                    < FontAwesomeIcon icon={faYoutube} />
+                                                </button></Link> : ''
+                                    }
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div>
+    )
 }
-
 export default Channel
