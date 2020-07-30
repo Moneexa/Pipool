ff6de528be71e262a4f768f56b0891f84c256ef0
Moneexa <14besemsyed@seecs.edu.pk>
2020-07-30 19:45:04 +0500

    little tweak in design

--- pwa/src/influence/pages/Overview/Overview.js
+++ pwa/src/influence/pages/Overview/Overview.js
@@ -1,6 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './Overview.css'
+import React from 'react';
+import './Overview.css';
 
 function Overview() {
     return (
