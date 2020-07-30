14c4e30428a5deabcb2ac8e3d24528ceadc192e1
Moneexa <14besemsyed@seecs.edu.pk>
2020-07-30 19:23:45 +0500

    social media channels

---
+++ server/channel/channelRoutes.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+var channelController = require('./channelController.js');
+var auth = require('../auth/auth')
+
+/*
+ * GET
+ */
+router.get('/:id', auth.verify, channelController.list);
+
+/*
+ * GET
+ */
+router.get('/', auth.verify, channelController.show);
+
+/*
+ * POST
+ */
+router.post('/', auth.verify, channelController.create);
+
+/*
+ * PUT
+ */
+router.put('/:id', auth.verify, channelController.update);
+
+/*
+ * DELETE
+ */
+router.delete('/:id', channelController.remove);
+router.post('/twitter/oauth/request_token', auth.verify, channelController.twitterOAuth);
+router.post('/twitter/oauth/', auth.verify, channelController.twitterPostOAuth);
+router.post('/youtube/oauth/', auth.verify, channelController.youtubeOAuth);
+router.post('/instagram/oauth/', auth.verify, channelController.InstaOAuth)
+router.post('/facebook/oauth/', auth.verify, channelController.FacebookOAuth)
+
+router.post('/tiktok/oauth/', auth.verify, channelController.TiktokPostOauth)
+
+module.exports = router;
