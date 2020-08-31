0441fd0914db4cea190f617d3947af458cede243
mohsini172 <mohsini172@gmail.com>
2020-08-31 14:46:08 +0500

    Added a library quill

---
+++ pwa/src/influence/pages/CampaignApply/CampaignApply.jsx
@@ -0,0 +1,78 @@
+import { useStoreActions, useStoreState } from 'easy-peasy';
+import React, { useEffect, useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import { useForm } from "react-hook-form";
+import styles from './CampaignApply.module.css';
+
+export default function CampaignInfo({ match }) {
+    const { register, handleSubmit, errors, watch } = useForm({ mode: "onBlur", reValidateMode: "onBlur" })
+
+    const campaignId = match.params.id;
+    const actv = useStoreState(state => state.campaign.actv)
+    const getCampaign = useStoreActions(actions => actions.campaign.getCampaign)
+    const postProposals = useStoreActions(actions => actions.proposals.postProposals);
+    const [proposalDoc, setProposalDoc] = useState('');
+    const proposals = useStoreState(state => state.proposals.actv)
+    useEffect(() => {
+        console.log(campaignId)
+        getCampaign(campaignId)
+    }, [getCampaign, campaignId])
+    function onNext(values) {
+        postProposals({ campaignId: campaignId, proposal: values.serviceDescription, cost: "", dateOfSubmission: "" })
+
+    }
+    return (
+        <div className={styles.container}>
+            <div className={styles.infoContainer}>
+                <h4 className={`mt-2 mx-4 mb-4 ${styles.title}`}>Submit a proposal</h4>
+                <div className={`mb-5 ${styles.card}`}>
+                    <div className="d-xs-none d-sm-flex">
+                        <div className={`flex-grow-1 ${styles.description}`}>
+                            <div className="d-flex flex-column">
+                                <div className={styles.heading}>
+                                    <h4 className="m-4">Campagin Description</h4>
+                                </div>
+                                <div className="p-4">
+                                    <h4 className={`d-inline-block ${styles.bold}`}>{actv.serviceName}</h4>
+                                    <div>
+                                        {actv.serviceDescription}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={`mb-5 ${styles.card}`} onSubmit={handleSubmit((values) => { console.log("Submit is working") })}>
+                    <div className="d-xs-none d-sm-flex">
+                        <div className={`flex-grow-1 ${styles.description}`}>
+                            <div className="d-flex flex-column">
+                                <div className={styles.heading}>
+                                    <h4 className="m-4">Terms</h4>
+                                </div>
+                                <div className="p-4">
+                                    <ReactQuill theme="snow" value={proposalDoc} onChange={setProposalDoc}/>
+                                </div>
+                            </div>
+                            <button>test</button>
+                            {/* <form className="row m-3" >
+
+                                <label>Write Proposal</label>
+                                <textarea
+
+                                    className="form-control form-control-user first-form p-3 m-3"
+                                    rows="30"
+                                    ref={register({ required: true })}
+
+                                    name="serviceDescription"
+                                    placeholder="Describe your product or service as if your audience is new to it. On the next steps, you'll be able to describe the content you'd like from our influencers. ">
+                                </textarea>
+                                <button type="submit" className="btn btn-primary btn-user text-white next-button m-3">Submit Proposal</button>
+
+                            </form> */}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>)
+}
