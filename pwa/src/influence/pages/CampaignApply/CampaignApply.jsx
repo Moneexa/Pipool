dc67749a6cab94b29d2b8e34262ded0ec1493a21
mohsini172 <mohsini172@gmail.com>
2020-09-01 19:11:04 +0500

    Custom video upload basics

--- pwa/src/influence/pages/CampaignApply/CampaignApply.jsx
+++ pwa/src/influence/pages/CampaignApply/CampaignApply.jsx
@@ -1,9 +1,38 @@
 import { useStoreActions, useStoreState } from 'easy-peasy';
 import React, { useEffect, useState } from 'react';
-import ReactQuill from 'react-quill';
+import ReactQuill, { Quill } from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import { useForm } from "react-hook-form";
 import styles from './CampaignApply.module.css';
+import { useRef } from 'react';
+
+// const Video = Quill.import('formats/video');
+// const Link = Quill.import('formats/link');
+// class CoustomVideo extends Video {
+//     static create(value) {
+//         const node = super.create(value);
+
+//         const video = document.createElement('video')
+//         video.setAttribute('controls', true);
+//         video.setAttribute('type', "video/mp4");
+//         video.setAttribute('style', "height: 200px; width: 100%");
+//         video.setAttribute('src', this.sanitize(value));
+//         node.appendChild(video);
+
+//         return node;
+//     }
+
+//     static sanitize(url) {
+//         return Link.sanitize(url);
+//     }
+// };
+// CoustomVideo.blotName = 'video';
+// CoustomVideo.className = 'ql-video';
+// CoustomVideo.tagName = 'DIV';
+
+// Quill.register('formats/video', CoustomVideo);
+
+
 export default function CampaignInfo({ match }) {
     const { register, handleSubmit, errors, watch } = useForm({ mode: "onBlur", reValidateMode: "onBlur" })
 
@@ -14,6 +43,25 @@ export default function CampaignInfo({ match }) {
     const [proposalDoc, setProposalDoc] = useState('');
     const proposals = useStoreState(state => state.proposals.actv)
 
+    const [config, _] = useState({
+        toolbar: {
+            container: [
+                [{ size: [] }],
+                ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+                [{ list: 'ordered' }, { list: 'bullet' }],
+                ['link', 'image', 'video'],
+                ['clean'],
+                ['code-block']
+            ],
+            handlers: {
+                video: async () => {
+                    uploadVideoClicked()
+                }
+              }
+        },
+    })
+    let reactQuillRef = useRef(null);
+
     useEffect(() => {
         console.log(campaignId)
         getCampaign(campaignId)
@@ -22,6 +70,12 @@ export default function CampaignInfo({ match }) {
         console.log(proposalDoc)
         postProposals({ campaignId: campaignId, proposal: proposalDoc, cost: "", dateOfSubmission: "" })
 
+    }
+    function uploadVideoClicked() {
+        debugger
+        const selection = reactQuillRef.current.getEditorSelection();
+        reactQuillRef.current.editor.insertEmbed(selection.index, 'video', "https://www.youtube.com/embed/GRFrVX3ghFo")
+        console.log("Upload video")
     }
     return (
         <div className={styles.container}>
@@ -52,22 +106,13 @@ export default function CampaignInfo({ match }) {
                                     <h4 className="m-4">Terms</h4>
                                 </div>
                                 <div className="p-4">
-                                    <ReactQuill theme="snow" value={proposalDoc} onChange={(e) => { setProposalDoc(e) }}
+                                    <ReactQuill theme="snow" 
+                                        value={proposalDoc} 
+                                        onChange={(e) => { setProposalDoc(e) }}
                                         name="proposal"
+                                        ref={reactQuillRef}
                                         // ref={register({required:true})}
-                                        modules={{
-                                            toolbar: {
-                                                container: [
-                                                    [{ size: [] }],
-                                                    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-                                                    [{ list: 'ordered' }, { list: 'bullet' }],
-                                                    ['link', 'image', 'video'],
-                                                    ['clean'],
-                                                    ['code-block']
-                                                ],
-
-                                            }
-                                        }}
+                                        modules={config}
                                     />
                                 </div>
                             </div>
