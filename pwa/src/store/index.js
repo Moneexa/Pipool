2bcd4152e85a62d437015d52962f53977352a3a6
Moneeza Syed <14besemsyed@seecs.edu.pk>
2020-11-14 19:13:12 +0500

    replaced with subject

--- pwa/src/store/index.js
+++ pwa/src/store/index.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react';
 
 export const useObservable = observable => {
     const [state, setState] = useState();
-
     useEffect(() => {
         const sub = observable.subscribe(setState);
         return () => sub.unsubscribe();
