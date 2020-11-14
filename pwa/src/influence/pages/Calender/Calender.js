0c6333a6a98c27050fb7cf7010b6a2610fd6cc7a
Moneeza Syed <14besemsyed@seecs.edu.pk>
2020-11-14 19:19:12 +0500

    added new cal lib

--- pwa/src/influence/pages/Calender/Calender.js
+++ pwa/src/influence/pages/Calender/Calender.js
@@ -2,8 +2,6 @@ import React from 'react'
 import './Calender.css'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
-
-
 class Calender extends React.Component {
     render() {
         return (<div className="calender-influence">
